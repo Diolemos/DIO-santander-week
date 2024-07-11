b8b73c145504a213bf19d7b631d0814011860154
Dio Lemos <pedro.devsc@gmail.com>
2024-07-11 11:11:34 -0300

    create models and add orm

---
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,41 @@
+package me.dio.domain.model;
+
+public class Feature {
+    private Long id;
+    private String icon;
+    private String description;
+
+    public Feature() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return this.icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Feature(Long id, String icon, String description) {
+        this.id = id;
+        this.icon = icon;
+        this.description = description;
+    }
+
+}
