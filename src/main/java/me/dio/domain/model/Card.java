d8b27269172e54cc22a6b1879f77f59fb754f55a
Dio Lemos <pedro.devsc@gmail.com>
2024-07-15 02:01:05 -0300

    update ORM

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -3,12 +3,13 @@ package me.dio.domain.model;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Table(name = "tb_card")
+@Entity(name = "tb_card")
 public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/me/dio/domain/model/Feature.java
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -1,5 +1,8 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
 public class Feature extends AbstractContent {
     public Feature() {
         super();
